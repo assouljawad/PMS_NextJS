7206948fc520ad9b17d259f6f050d6957f1c4a73
assouljawad <assouljawad823@gmail.com>
2024-05-23 20:44:30 +0100

    add header and style it with tailwind css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/frontend/homepage/header";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="bg-[#F4F4F4]">
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- components/frontend/homepage/header.tsx
+++ components/frontend/homepage/header.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Link from 'next/link'
+import { IoNewspaperOutline, IoSearch } from "react-icons/io5";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
+
+
 
 function Header() {
   return (
-    <div>Header</div>
+   <div className='bg-white mt-6 w-full'>
+    <div className='flex justify-between items-center mx-auto p-4 w-3/4'>
+      <div className='flex gap-2 items-center'>
+      <IoNewspaperOutline className='text-4xl' />
+      <span className='text-3xl font-bold'>BlogPost</span>
+      </div>  
+      <div className='bg-[#F4F4F4] p-2 rounded-full flex items-center justify-between gap-2 w-2/6'>
+        <input type="text" placeholder='Search' className='rounded-full bg-[#F4F4F4] w-full focus:outline-none'/>
+        <IoSearch className='text-2xl text-gray-500 cursor-pointer'/>
+      </div>
+      <div className='flex gap-3'>
+        <nav className='flex gap-2'>
+          <Link href={"/"} className='font-bold'>Home</Link>
+          <span className='font-bold flex items-center'>
+            More
+            <MdOutlineKeyboardArrowDown />
+            </span>
+          <Link href={"/contact"} className='font-bold'>Contact</Link>
+        </nav>
+        <MdOutlineDarkMode className='cursor-pointer text-2xl'/>
+      </div>
+    </div>
+   </div>
   )
 }
 
